1c301c0c27e3aa9c3441708e787fa08f0adc7760
Pedro Pazello <email@pedropazello.com>
2021-10-11 08:52:49 -0300

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM golang:1.17.2
 
 RUN go mod init
+RUN go get -u -v \
+        github.com/antchfx/htmlquery
 
 RUN mkdir /app
 WORKDIR /app
--- src/americanas/get_prices.go
+++ src/americanas/get_prices.go
@@ -1,49 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "github.com/antchfx/htmlquery"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/antchfx/htmlquery"
 )
 
 func main() {
-    url := "https://www.americanas.com.br/produto/148004923"
+	url := "https://www.americanas.com.br/produto/148004923"
 
-    req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", url, nil)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    req.Header = http.Header{
-        "Host": []string{"www.americanas.com.br"},
-        "User-Agent": []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0"},
-        "Accept-Language": []string{"pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3"},
-    }
+	req.Header = http.Header{
+		"Host":            []string{"www.americanas.com.br"},
+		"User-Agent":      []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0"},
+		"Accept-Language": []string{"pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3"},
+	}
 
-    client := http.Client{}
-    resp, err := client.Do(req)
+	client := http.Client{}
+	resp, err := client.Do(req)
 
-    if (err != nil) {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 
-    if (err != nil) {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    doc, err := htmlquery.Parse(strings.NewReader(string(body)))
+	doc, err := htmlquery.Parse(strings.NewReader(string(body)))
 
-    if (err != nil) {
-      panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    a := htmlquery.FindOne(doc, "//div[1]/div/div/main/div[3]/div[2]/div[1]/div[1]/div")
+	a := htmlquery.FindOne(doc, "//div[1]/div/div/main/div[3]/div[2]/div[1]/div[1]/div")
 
-    fmt.Printf(htmlquery.InnerText(a))
+	fmt.Printf(htmlquery.InnerText(a))
 }
